481c2d0d56e9add20de8a1cf792eeda1d90be38b
Felipe blini <Felipe.blini@gmail.com>
2019-12-19 01:05:01 -0300

    added /Index.vue on all imports

--- pages/index.vue
+++ pages/index.vue
@@ -15,13 +15,13 @@
 </template>
 
 <script>
-import LingoHero from '~/components/Hero'
-import LingoMiniBio from '~/components/Minibio'
+import LingoHero from '~/components/Hero/Index.vue'
+import LingoMiniBio from '~/components/Minibio/Index.vue'
 import LingoAbout from '~/components/About/Index.vue'
-import LingoServices from '~/components/OurServices'
-import LingoTestemonials from '~/components/Testemonials'
-import LingoPartners from '~/components/Partners'
-import LingoMapaMundi from '~/components/MapaMundi'
+import LingoServices from '~/components/OurServices/Index.vue'
+import LingoTestemonials from '~/components/Testemonials/Index.vue'
+import LingoPartners from '~/components/Partners/Index.vue'
+import LingoMapaMundi from '~/components/MapaMundi/Index.vue'
 import LingoFooter from '~/components/Footer/Index.vue'
 
 export default {
